a754cbe3223abf7a22d4e67ab261e4887ccce567
Mohseghir <baghaoui@homtail.com>
2023-06-14 22:51:15 +0100

    detail and list serializers

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from shop.views import CategoryAPIViewset, ProductViewset, ArticleViewset
+
+router = routers.SimpleRouter()
+# Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
+# afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
+router.register('category', CategoryAPIViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
+router.register('article', ArticleViewset, basename='article')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls))
 ]
